66e32e280c1e24c0f05fd26de6c64065241797ce
Joelbabay <babayjoel7@gmail.com>
2024-08-16 18:59:25 +0300

    ajout magnificpopup js

--- src/Controller/IntradayController.php
+++ src/Controller/IntradayController.php
@@ -6,10 +6,49 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/intraday')]
 class IntradayController extends AbstractController
 {
-    #[Route('/intraday', name: 'intraday')]
+    #[Route('/', name: 'intraday')]
     public function index(): Response
+    {
+        return $this->redirectToRoute('intraday_presentation');
+    }
+
+    #[Route('/presentation', name: 'intraday_presentation')]
+    public function intraday_presentation(): Response
+    {
+        return $this->render('intraday/index.html.twig', [
+            'controller_name' => 'IntradayController',
+        ]);
+    }
+
+    #[Route('/methode', name: 'intraday_methode')]
+    public function intraday_methode(): Response
+    {
+        return $this->render('intraday/index.html.twig', [
+            'controller_name' => 'IntradayController',
+        ]);
+    }
+
+    #[Route('/bibliotheque', name: 'intraday_bibliotheque')]
+    public function intraday_bibliotheque(): Response
+    {
+        return $this->render('intraday/index.html.twig', [
+            'controller_name' => 'IntradayController',
+        ]);
+    }
+
+    #[Route('/outils', name: 'intraday_outils')]
+    public function intraday_outils(): Response
+    {
+        return $this->render('intraday/index.html.twig', [
+            'controller_name' => 'IntradayController',
+        ]);
+    }
+
+    #[Route('/gestions', name: 'intraday_gestion')]
+    public function intraday_gestion(): Response
     {
         return $this->render('intraday/index.html.twig', [
             'controller_name' => 'IntradayController',
