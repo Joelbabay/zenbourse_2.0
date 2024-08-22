ab070d555479d261a585868db0f11c079650d8b3
Joelbabay <babayjoel7@gmail.com>
2024-08-22 20:16:37 +0300

    modif interrest investisor

--- src/Controller/Admin/InterestedUsersCrudController.php
+++ src/Controller/Admin/InterestedUsersCrudController.php
@@ -12,14 +12,12 @@ use Doctrine\ORM\QueryBuilder;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
-use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Dto\SearchDto;
 use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Orm\EntityRepository as OrmEntityRepository;
-use Symfony\Component\HttpFoundation\Response;
 
 class InterestedUsersCrudController extends AbstractCrudController
 {
@@ -48,12 +46,8 @@ class InterestedUsersCrudController extends AbstractCrudController
 
     public function configureActions(Actions $actions): Actions
     {
-        $validateAction = Action::new('validate', 'Valider')
-            ->linkToCrudAction('validateUser')
-            ->setCssClass('btn btn-success text-white');
-
         return $actions
-            ->add(Crud::PAGE_INDEX, $validateAction);
+            ->disable(Action::NEW, Action::DELETE, Action::EDIT);
     }
 
     public function createIndexQueryBuilder(SearchDto $searchDto, EntityDto $entityDto, FieldCollection $fields, FilterCollection $filters): QueryBuilder
@@ -74,19 +68,5 @@ class InterestedUsersCrudController extends AbstractCrudController
             TextField::new('email'),
             DateTimeField::new('createdAt', 'Date de demande'),
         ];
-    }
-
-    public function validateUser(AdminContext $context,): Response
-    {
-        $user = $context->getEntity()->getInstance();
-        if ($user instanceof User) {
-            $user->setInterestedInInvestorMethod(false);
-            $user->setIsInvestisseur(true);
-            $this->entityManager->flush();
-        }
-
-        $this->addFlash('success', "L'utilisateur a été validé.");
-
-        return $this->redirectToRoute('admin');
     }
 }
