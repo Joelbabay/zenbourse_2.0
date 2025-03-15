a74ec24173110a82154ba45bacbe35b31675e155
joelbabay <babayjoel7@gmail.com>
2025-03-15 14:01:14 +0300

    modif homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,37 +12,6 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(): Response
     {
-        $this->redirectToRoute('home');
-        return $this->render('home/home.html.twig', []);
-    }
-
-    #[Route('/methodes', name: 'home_methodes')]
-    public function methodes(): Response
-    {
-        return $this->render('home/methodes.html.twig');
-    }
-
-    #[Route('/le-perdant', name: 'home_perdant')]
-    public function lePerdant(): Response
-    {
-        return $this->render('home/le_perdant.html.twig');
-    }
-
-    #[Route('/citation', name: 'home_citation')]
-    public function citation(): Response
-    {
-        return $this->render('home/citation.html.twig');
-    }
-
-    #[Route('/bien-debuter', name: 'home_bien_debuter')]
-    public function bienDebuter(): Response
-    {
-        return $this->render('home/bien_debuter.html.twig');
-    }
-
-    #[Route('/performance', name: 'home_performance')]
-    public function performance(): Response
-    {
-        return $this->render('home/performance.html.twig');
+        return $this->redirect('/home');
     }
 }
