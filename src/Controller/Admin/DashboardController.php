634d3375806c867af846517e098722f94e367174
joelbabay <babayjoel7@gmail.com>
2025-04-26 16:49:44 +0300

    IMAGE MODIF

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -429,7 +429,7 @@ class DashboardController extends AbstractDashboardController
                             contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells', 'tableProperties', 'tableCellProperties']
                         },
                         simpleUpload: {
-                            uploadUrl: '/uploadImage',
+                            uploadUrl: '/uploads/',
                             headers: {
                                 'X-CSRF-TOKEN': 'CSRF-Token' 
                             }
