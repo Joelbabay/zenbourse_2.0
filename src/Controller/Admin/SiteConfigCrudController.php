d3828784012e188372b88e44a504215cf2882a7a
Joelbabay <babayjoel7@gmail.com>
2026-03-15 22:35:54 +0300

    site config edit

--- src/Controller/Admin/SiteConfigCrudController.php
+++ src/Controller/Admin/SiteConfigCrudController.php
@@ -4,6 +4,8 @@
 namespace App\Controller\Admin;
 
 use App\Entity\SiteConfig;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
@@ -14,6 +16,29 @@ class SiteConfigCrudController extends AbstractCrudController
     public static function getEntityFqcn(): string
     {
         return SiteConfig::class;
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+
+        return $actions
+            ->update(Crud::PAGE_INDEX, Action::NEW, function (Action $action) {
+                return $action->setIcon('fa fa-plus')->setLabel('Créer');
+            })
+
+            ->update(Crud::PAGE_INDEX, Action::EDIT, function (Action $action) {
+                return $action
+                    ->setIcon('fas fa-edit')
+                    ->setLabel(false)
+                    ->addCssClass('btn btn-link');
+            })
+            // Mise à jour de l'action de suppression pour utiliser une icône spécifique
+            ->update(Crud::PAGE_INDEX, Action::DELETE, function (Action $action) {
+                return $action
+                    ->setIcon('fas fa-trash')
+                    ->setLabel(false)
+                    ->addCssClass('btn btn-link text-danger');
+            });
     }
 
     public function configureCrud(Crud $crud): Crud
